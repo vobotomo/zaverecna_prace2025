737f0c5330c0b7b92cf5ad7a0ca029cf656d0264
vobotomo <vobornyt@gmail.com>
2025-05-29 08:28:21 +0000

    account toString

--- src/Account/Account.java
+++ src/Account/Account.java
@@ -101,20 +101,28 @@ public class Account implements Comparable<Account> {
     }
 
     @Override
-    public String toString() {
-        return "\n--- Account Information ---" +
-                "\nID: " + id +
-                "\nName: " + ownerName + " " + ownerSurname +
-                "\nUsername: " + username +
-                "\nEmail: " + email +
-                "\nPhone: " + phoneNumber +
-                "\nBalance: " + balance +
-                "\nAccount Type: " + accountType +
-                "\nInterest Rate: " + interestRate +
-                "\nTransactions: " + transactions.size() + " total" +
-                "\n---------------------------";
+public String toString() {
+    String result = "\n--- Account Information ---" +
+                    "\nID: " + id +
+                    "\nName: " + ownerName + " " + ownerSurname +
+                    "\nUsername: " + username +
+                    "\nEmail: " + email +
+                    "\nPhone: " + phoneNumber +
+                    "\nBalance: " + balance +
+                    "\nAccount Type: " + accountType +
+                    "\nInterest Rate: " + interestRate +
+                    "\nTransactions: " + transactions.size() + " total";
+
+    if (!transactions.isEmpty()) {
+        result += "\n--- Transaction History ---";
+        for (Transaction t : transactions) {
+            result += "\n" + t.toString();
+        }
     }
 
+    result += "\n---------------------------";
+    return result;
+}
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
